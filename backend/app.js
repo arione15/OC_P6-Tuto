ab12ed86380a4a5c5510d3e34cd2fb6ae20c973c
Massine <massinoueslati@gmail.com156>
2021-08-26 12:27:09 +0200

    ajout de midlewares de test

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,27 @@ const expr= require('express');
 
 const monAppExpress = expr(); // création d'une application express
 
-monAppExpress.use((req, resp)=>{ //faire une requête/réponse (vers le serveur via) Express
-    resp.json({"message": "Coucoule !"});
+//monAppExpress.use((req, resp)=>{ //faire une requête/réponse (vers le serveur via) Express
+//    resp.json({"message": "Coucoule !"});
+//});
+
+//Ajout des midelwares
+monAppExpress.use((req, resp, next)=>{
+    console.log("Requête reçue");
+    next();
 });
+monAppExpress.use((req, resp, next)=>{
+    resp.status(201);
+    next();
+});
+monAppExpress.use((req, resp, next)=>{
+    resp.json({"message": "Youpi !!! La requête a bien été reçue !"});
+    next();
+});
+monAppExpress.use((req, resp, next)=>{
+    console.log("Réponse envoyée avec succès !");
+});
+
+
 
 module.exports = monAppExpress;
--- readme.md
+++ readme.md
@@ -1,1 +1,11 @@
-## Tuto Projet 6 : Passez au Full stack avec Node.js, Express et MongoDB
+# Tuto Projet 6 : Passez au Full stack avec Node.js, Express et MongoDB
+
+## I. Environnement de développement
+
+* Node.js
+* Express
+* MongoDB - Mongoose
+## II. Points à retenir
+ 
+1. D
+    <ins>ATTENTION :</ins> `$_SESSION[$this->nom]` est un chiffre. Mettre (int) pour éviter erreur.
